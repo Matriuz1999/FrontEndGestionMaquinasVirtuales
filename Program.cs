79bfca5bd6b93b08e84e74e951f889090fd33880
juan david herrera molina <juand_0199@hotmail.com>
2025-05-07 17:28:51 -0500

    Adaptado para azure

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Blazored.LocalStorage;
 using FrontEndGestionMaquinasVirtuales.Servicios;
+using Microsoft.Azure.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +26,7 @@ builder.Services.AddSingleton<SignalRService>();
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiSettings:BaseUrl"] ?? "https://localhost:5001/api") }
 );
+builder.Services.AddSignalR().AddAzureSignalR("Endpoint=https://apigestionmaquinasvirtuales.service.signalr.net;AccessKey=3xLHyuOmuR2YK4pGUVJ7Xsp5vBQmRYj4E1tA541QJr799cdAjpaaJQQJ99BEACBsN54XJ3w3AAAAASRSNqHc;Version=1.0;");
 
 var app = builder.Build();
 
